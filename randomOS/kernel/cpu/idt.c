4fabeee32c3b06b11cdb080fc75d8ea26b6d68af
sarvesh10491 <sarvesh10491@gmail.com>
2020-12-29 23:55:16 +0000

    Added IRQ code

--- randomOS/kernel/cpu/idt.c
+++ randomOS/kernel/cpu/idt.c
@@ -25,38 +25,82 @@ static void __init_idt(){
 
    memory_set(&idt_entries, 0, sizeof(idt_entry_t)*256);
 
-   __idt_set_gate(0, (u32int)isr0 , 0x08, 0x8E);
-   __idt_set_gate(1, (u32int)isr1 , 0x08, 0x8E);
-   __idt_set_gate(2, (u32int)isr2 , 0x08, 0x8E);
-   __idt_set_gate(3, (u32int)isr3 , 0x08, 0x8E);
-   __idt_set_gate(4, (u32int)isr4 , 0x08, 0x8E);
-   __idt_set_gate(5, (u32int)isr5 , 0x08, 0x8E);
-   __idt_set_gate(6, (u32int)isr6 , 0x08, 0x8E);
-   __idt_set_gate(7, (u32int)isr7 , 0x08, 0x8E);
-   __idt_set_gate(8, (u32int)isr8 , 0x08, 0x8E);
-   __idt_set_gate(9, (u32int)isr9 , 0x08, 0x8E);
-   __idt_set_gate(10, (u32int)isr10 , 0x08, 0x8E);
-   __idt_set_gate(11, (u32int)isr11 , 0x08, 0x8E);
-   __idt_set_gate(12, (u32int)isr12 , 0x08, 0x8E);
-   __idt_set_gate(13, (u32int)isr13 , 0x08, 0x8E);
-   __idt_set_gate(14, (u32int)isr14 , 0x08, 0x8E);
-   __idt_set_gate(15, (u32int)isr15 , 0x08, 0x8E);
-   __idt_set_gate(16, (u32int)isr16 , 0x08, 0x8E);
-   __idt_set_gate(17, (u32int)isr17 , 0x08, 0x8E);
-   __idt_set_gate(18, (u32int)isr18 , 0x08, 0x8E);
-   __idt_set_gate(19, (u32int)isr19 , 0x08, 0x8E);
-   __idt_set_gate(20, (u32int)isr20 , 0x08, 0x8E);
-   __idt_set_gate(21, (u32int)isr21 , 0x08, 0x8E);
-   __idt_set_gate(22, (u32int)isr22 , 0x08, 0x8E);
-   __idt_set_gate(23, (u32int)isr23 , 0x08, 0x8E);
-   __idt_set_gate(24, (u32int)isr24 , 0x08, 0x8E);
-   __idt_set_gate(25, (u32int)isr25 , 0x08, 0x8E);
-   __idt_set_gate(26, (u32int)isr26 , 0x08, 0x8E);
-   __idt_set_gate(27, (u32int)isr27 , 0x08, 0x8E);
-   __idt_set_gate(28, (u32int)isr28 , 0x08, 0x8E);
-   __idt_set_gate(29, (u32int)isr29 , 0x08, 0x8E);
-   __idt_set_gate(30, (u32int)isr30 , 0x08, 0x8E);
-   __idt_set_gate(31, (u32int)isr31 , 0x08, 0x8E);
+   __idt_set_gate(0, (u32int)isr0, 0x08, 0x8E);
+   __idt_set_gate(1, (u32int)isr1, 0x08, 0x8E);
+   __idt_set_gate(2, (u32int)isr2, 0x08, 0x8E);
+   __idt_set_gate(3, (u32int)isr3, 0x08, 0x8E);
+   __idt_set_gate(4, (u32int)isr4, 0x08, 0x8E);
+   __idt_set_gate(5, (u32int)isr5, 0x08, 0x8E);
+   __idt_set_gate(6, (u32int)isr6, 0x08, 0x8E);
+   __idt_set_gate(7, (u32int)isr7, 0x08, 0x8E);
+   __idt_set_gate(8, (u32int)isr8, 0x08, 0x8E);
+   __idt_set_gate(9, (u32int)isr9, 0x08, 0x8E);
+   __idt_set_gate(10, (u32int)isr10, 0x08, 0x8E);
+   __idt_set_gate(11, (u32int)isr11, 0x08, 0x8E);
+   __idt_set_gate(12, (u32int)isr12, 0x08, 0x8E);
+   __idt_set_gate(13, (u32int)isr13, 0x08, 0x8E);
+   __idt_set_gate(14, (u32int)isr14, 0x08, 0x8E);
+   __idt_set_gate(15, (u32int)isr15, 0x08, 0x8E);
+   __idt_set_gate(16, (u32int)isr16, 0x08, 0x8E);
+   __idt_set_gate(17, (u32int)isr17, 0x08, 0x8E);
+   __idt_set_gate(18, (u32int)isr18, 0x08, 0x8E);
+   __idt_set_gate(19, (u32int)isr19, 0x08, 0x8E);
+   __idt_set_gate(20, (u32int)isr20, 0x08, 0x8E);
+   __idt_set_gate(21, (u32int)isr21, 0x08, 0x8E);
+   __idt_set_gate(22, (u32int)isr22, 0x08, 0x8E);
+   __idt_set_gate(23, (u32int)isr23, 0x08, 0x8E);
+   __idt_set_gate(24, (u32int)isr24, 0x08, 0x8E);
+   __idt_set_gate(25, (u32int)isr25, 0x08, 0x8E);
+   __idt_set_gate(26, (u32int)isr26, 0x08, 0x8E);
+   __idt_set_gate(27, (u32int)isr27, 0x08, 0x8E);
+   __idt_set_gate(28, (u32int)isr28, 0x08, 0x8E);
+   __idt_set_gate(29, (u32int)isr29, 0x08, 0x8E);
+   __idt_set_gate(30, (u32int)isr30, 0x08, 0x8E);
+   __idt_set_gate(31, (u32int)isr31, 0x08, 0x8E);
+
+/* Ports
+	*	         PIC1	PIC2
+	* Command   0x20	0xA0
+	* Data	   0x21	0xA1
+*/
+// ICW1 - begin initialization
+   port_byte_out(0x20, 0x11);
+   port_byte_out(0xA0, 0x11);
+
+// ICW2 - remap offset address of IDT
+// In x86 protected mode, we have to remap the PICs beyond 0x20 because Intel have designated the first 32 interrupts as "reserved" for cpu exceptions
+   port_byte_out(0x21, 0x20);
+   port_byte_out(0xA1, 0x28);
+
+// ICW3 - setup cascading
+   port_byte_out(0x21, 0x04);
+   port_byte_out(0xA1, 0x02);
+
+// ICW4 - environment info
+   port_byte_out(0x21, 0x01);
+   port_byte_out(0xA1, 0x01);
+
+// mask interrupts
+   port_byte_out(0x21, 0x0);
+   port_byte_out(0xA1, 0x0);
+
+
+   __idt_set_gate(32, (u32int)irq0, 0x08, 0x8E);
+   __idt_set_gate(33, (u32int)irq1, 0x08, 0x8E);
+   __idt_set_gate(34, (u32int)irq2, 0x08, 0x8E);
+   __idt_set_gate(35, (u32int)irq3, 0x08, 0x8E);
+   __idt_set_gate(36, (u32int)irq4, 0x08, 0x8E);
+   __idt_set_gate(37, (u32int)irq5, 0x08, 0x8E);
+   __idt_set_gate(38, (u32int)irq6, 0x08, 0x8E);
+   __idt_set_gate(39, (u32int)irq7, 0x08, 0x8E);
+   __idt_set_gate(40, (u32int)irq8, 0x08, 0x8E);
+   __idt_set_gate(41, (u32int)irq9, 0x08, 0x8E);
+   __idt_set_gate(42, (u32int)irq10, 0x08, 0x8E);
+   __idt_set_gate(43, (u32int)irq11, 0x08, 0x8E);
+   __idt_set_gate(44, (u32int)irq12, 0x08, 0x8E);
+   __idt_set_gate(45, (u32int)irq13, 0x08, 0x8E);
+   __idt_set_gate(46, (u32int)irq14, 0x08, 0x8E);
+   __idt_set_gate(47, (u32int)irq15, 0x08, 0x8E);
 
 
    idt_flush((u32int)&idt_ptr);
--- randomOS/kernel/cpu/interrupt.c
+++ randomOS/kernel/cpu/interrupt.c
@@ -1,6 +1,6 @@
 #include "interrupt.h"
 
-// This gets called from our ASM interrupt handler stub.
+// This gets called from our ASM interrupt handler stub for ISR.
 void isr_handler(registers_t r){
    kprint("Received interrupt : #");
    char s[3];
@@ -9,4 +9,23 @@ void isr_handler(registers_t r){
    kprint("  [");
    kprint(exception_messages[r.int_no]);
    kprint("]\n");
+}
+
+// This gets called from our ASM interrupt handler stub for IRQ.
+void irq_handler(registers_t r){
+   // After every interrupt we need to send an EOI to the PICs or they will not send another interrupt again
+   if(r.int_no >= 40) 
+      port_byte_out(0xA0, 0x20);    // slave
+   port_byte_out(0x20, 0x20);       // master
+
+   // Handle the interrupt in a more modular way. Select registered handler from list & call it.
+   if(interrupt_handlers[r.int_no] != 0){
+      isr_t handler = interrupt_handlers[r.int_no];
+      handler(r);
+   }
+}
+
+
+void register_interrupt_handler(u8int n, isr_t handler) {
+    interrupt_handlers[n] = handler;
 }
